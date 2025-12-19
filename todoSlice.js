639591bff4dd133a0ebe78d8f1f5d88020c96c75
Ashok Sahani <ashoksahani2004@gmail.com>
2025-12-19 20:21:26 +0530

    Add files via upload

---
+++ todoSlice.js
@@ -0,0 +1,35 @@
+// Resducers
+
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const initialState = {
+    todos: [{id:"abc", task:"todo-task", isDone:false}],
+};
+
+export const todoSlice = createSlice({
+    name:"todo",
+    initialState,
+    reducers: {
+        addTodo: (state, action) =>{
+            const newTodo ={
+                id: nanoid(),
+                task: action.payload,
+                isDone: false,
+            };
+            state.todos.push(newTodo);  // dirrect mutation
+        },
+        deleteTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id != action.payload);
+        },
+        marksAsDone: (state, action) => {
+             state.todos = state.todos.map((todo) => {
+            if(todo.id === action.payload){
+                todo.isDone = true;
+            }
+           });
+        },
+    },
+});
+
+export const { addTodo, deleteTodo, markAsDone } = todoSlice.actions;
+export default todoSlice.reducer;
